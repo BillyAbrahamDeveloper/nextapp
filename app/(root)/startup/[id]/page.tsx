ef6c65a1755ad14edc5f91ab2bd7081b52560def
BillyAbrahamDeveloper <contact@billydev.me>
2024-11-13 11:09:55 -0600

    fetching data for details page

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,5 +1,25 @@
-const StartupIDPage = () => {
-  return <div>Enter</div>;
+import { client } from '@/sanity/lib/client';
+import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
+import { notFound } from 'next/navigation';
+
+export const experimental_ppr = true;
+
+const StartupIDPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const id = (await params).id;
+
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+  if (!post) return notFound();
+
+  return (
+    <>
+      <h1>{post.title}</h1>
+    </>
+  );
 };
 
 export default StartupIDPage;
